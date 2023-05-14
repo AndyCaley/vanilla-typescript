a1939797a4f4593d6096164a8ec82c59540c4d8b
AndyCaley <acaley@gmail.com>
2023-05-14 17:34:45 +0000

    feat: add blessed-contrib example

--- src/Hello.ts
+++ src/Hello.ts
@@ -1,1 +1,26 @@
-console.log("Hola")
+import blessed from 'blessed';
+import contrib from 'blessed-contrib';
+
+var screen = blessed.screen();
+var line = contrib.line(
+    { style:
+      { line: "yellow"
+      , text: "green"
+      , baseline: "black"}
+    , xLabelPadding: 3
+    , xPadding: 5
+    , label: 'Title'});
+
+var data = {
+    x: ['t1', 't2', 't3', 't4'],
+    y: [5, 1, 7, 5]
+};
+
+screen.append(line);
+line.setData([data]);
+
+screen.key(['escape', 'q'], (ch,key) => {
+    return process.exit(0);
+});
+
+screen.render();
